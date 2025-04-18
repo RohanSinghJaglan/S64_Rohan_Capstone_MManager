d7b79aeb08c01b3f803c44fd317880ed67c67005
Rohan Singh Jaglan <rohan.singh@kalvium.community>
2025-04-18 10:12:22 +0530

    Resolved Issues

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -47,35 +47,7 @@ const addDoctor = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
- 
-// API for getting all doctors
-const adminDashboard = async (req, res) => {
-    try {
-
-        const doctors = await doctorModel.find({})
-        const users = await userModel.find({})
-        const appointments = await appointmentModel.find({})
-
-        const dashData = {
-            doctors: doctors.length,
-            appointments: appointments.length,
-            patients: users.length,
-            latestAppointments: appointments.reverse()
-        }
-
-        res.json({ success: true, dashData })
-
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
 
 export {
     addDoctor,
-    allDoctors,
-    adminDashboard
-
-
-
 }
